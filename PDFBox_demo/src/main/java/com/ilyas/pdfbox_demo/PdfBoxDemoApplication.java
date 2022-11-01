c0d94d504e7f797aa362e8a248f6de0a45d560ab
aisraja <rajashekar.betha@aiswebnet.com>
2022-11-01 15:42:24 -0500

    PDF BOX
    PDF Box

---
+++ PDFBox_demo/src/main/java/com/ilyas/pdfbox_demo/PdfBoxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ilyas.pdfbox_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfBoxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfBoxDemoApplication.class, args);
+    }
+
+}
